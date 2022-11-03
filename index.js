041723e4378eb0135cccc913e735cf35535d85e4
Tobi Balogun <tbalogun146@gmail.com>
2022-11-03 02:30:34 +0100

    array lessons

---
+++ index.js
@@ -0,0 +1,32 @@
+// const number = [3,4];
+
+// // number.push(5, 6, 7 )
+
+// const water = number.push(5, 6, 7)
+// console.log(water)cont
+
+
+// const courses = [
+//     {id : 1, name: 'chm101'}
+//     {id :2,  name:   'chm101'}
+// 
+
+// humans.find(function(human){
+//     return  human.name === 'divine'
+// })
+
+// // const mand = humans.find(gender())
+
+// // console.log(mand)
+// // const numbers = [1,2,3,4];
+// // numbers.indexOf(1);
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let index = fruits.indexOf("Orange",3);
+// console.log(index)
+
+const number = [1,2,3]
+const child = number.map((value) => {
+   return value * 2;
+})
+
+console.log (child)
